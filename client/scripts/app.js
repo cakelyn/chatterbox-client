9165aaa55aca8ecfb07a5fc1157e677169667b1e
Katelyn Stamas <cakelyn@gmail.com>
2017-10-20 16:43:17 -0400

    completed bower requirements

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,29 @@
 // YOUR CODE HERE:
+
+var app = {
+
+  init: function() {
+
+  },
+
+  send: function() {
+
+  },
+
+  fetch: function() {
+
+  },
+
+  clearMessages: function() {
+
+  },
+
+  renderMessage: function() {
+
+  },
+
+  renderRoom: function() {
+
+  }
+
+};
