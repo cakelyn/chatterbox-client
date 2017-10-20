0c02681022430b7f2e61a067d024b53483e0b563
Katelyn Stamas <cakelyn@gmail.com>
2017-10-20 18:42:51 -0400

    safe commit

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,5 +1,8 @@
-// YOUR CODE HERE:
+// YOUR CODE HER$.
+/*
+$.ajax({ url: http://parse.CAMPUS.hackreactor.com/chatterbox/classes/messages where CAMPUS }
 
+*/
 var app = {
 
   init: function() {
@@ -7,22 +10,41 @@ var app = {
   },
 
   send: function() {
+    // submit a POST request via ajax
+    // should send correct method along with request
 
   },
 
   fetch: function() {
+    // should submit a GET request via ajax
 
   },
 
   clearMessages: function() {
+    // should be able to clear messages from the DOM
 
   },
 
   renderMessage: function() {
+    // should be able to add messages to the DOM
+
+    // run any input through sanitize-html
+    // if it returns all fine, send input to send method
 
   },
 
   renderRoom: function() {
+    // should be able to add rooms to the DOM
+
+  },
+
+  handleUsernameClick: function() {
+    // should add a friend
+
+  },
+
+  handleSubmit: function() {
+    // should try to send a message upon clicking submit
 
   }
 
