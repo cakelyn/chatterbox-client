bc2954321a62c80aa1617e2e53eeda1167c9622e
Katelyn Stamas <cakelyn@gmail.com>
2017-10-20 19:14:54 -0400

    set up send and sanitizeHTML

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,17 +1,55 @@
 // YOUR CODE HER$.
 /*
-$.ajax({ url: http://parse.CAMPUS.hackreactor.com/chatterbox/classes/messages where CAMPUS }
+$.ajax({
+  // This is the url you should use to communicate with the parse API server.
+  url: 'http://parse.hrr.hackreactor.com/chatterbox/classes/messages',
+  type: 'POST',
+  data: JSON.stringify(message),
+  contentType: 'application/json',
+  success: function (data) {
+    console.log('chatterbox: Message sent');
+  },
+  error: function (data) {
+    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+    console.error('chatterbox: Failed to send message', data);
+  }
+});
 
 */
+
+$(doument).ready(function(){
+
+  $('#sumbit').click(function() {
+    // take data that was input by user in #input
+    var dirtyData = $('#input').text();
+    // run dirtyData through sanitizeHTML
+    // store that sanitized data into a variable
+    var cleanData = sanitizeHTML(dirtyData);
+    // send that variable to send method
+    app.send(cleanData).bind(app);
+  });
+
+});
+
 var app = {
 
   init: function() {
 
   },
 
-  send: function() {
+  send: function(postData) {
     // submit a POST request via ajax
     // should send correct method along with request
+
+    $.ajax({
+      url: 'http://parse.hrr.hackreactor.com/chatterbox/classes/messages',
+      type: 'POST',
+      data: JSON.stringify(postData),
+      contentType: 'application/json',
+      success: function (data) {
+        // send data to renderMessage or run renderMessage
+      }
+    });
 
   },
 
@@ -25,11 +63,16 @@ var app = {
 
   },
 
+  // sanitizeHTML: function(dirtyData) {
+  //   // var sanitizeHtml = require('sanitize-html');
+  //   // run any input through sanitize-html
+  //   // if it returns all fine, send postData to send method
+
+  //   // retun cleanPostData
+  // },
+
   renderMessage: function() {
     // should be able to add messages to the DOM
-
-    // run any input through sanitize-html
-    // if it returns all fine, send input to send method
 
   },
 
@@ -49,3 +92,5 @@ var app = {
   }
 
 };
+
+var sanitizeHtml = require('sanitize-html');
